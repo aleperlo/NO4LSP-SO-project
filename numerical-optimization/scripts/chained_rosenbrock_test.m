a1e32f64e5c93ed4a10130a5e54f06982cdcb0b2
Mattia Molinari <matty.molinari@gmail.com>
2024-11-30 11:49:23 +0100

    Complete tests for chained rosenbrock

--- numerical-optimization/scripts/chained_rosenbrock_test.m
+++ numerical-optimization/scripts/chained_rosenbrock_test.m
@@ -14,29 +14,28 @@ f = @chained_rosenbrock;
 gradf = @chained_rosenbrock_gradf;
 Hessf = @chained_rosenbrock_Hessf;
 
+load('data/forcing_terms.mat');
 beta = 10^-3;
 kmax = 5*10^3;
 tolgrad = 1e-8;
 c1 = 1e-4;
 rho = 0.8;
-btmax = 50;
+btmax = 100;
 max_chol_iter = 100;
+pcg_maxit = 1000;
 
 
 [xk, fk, ~, k, ~, ~] = ...
     modified_newton(x0, f, gradf, Hessf, beta, kmax, tolgrad, c1, rho, btmax, max_chol_iter);
 
-disp(['Modified Newton converged in ', num2str(k), ' iterations.'])
+disp(['MODIFIED Newton converged in ', num2str(k), ' iterations.'])
 disp(['The x value at the solution is ', mat2str(xk), '.'])
 disp(['The function value at the solution is ', mat2str(fk), '.'])
 
-fk(1)
-
-fk(10^3)
 
 [xk, fk, ~, k, ~, ~] = ...
-    truncated_newton(x0, f, gradf, Hessf, beta, kmax, tolgrad, c1, rho, btmax, max_chol_iter);
+    truncated_newton(x0, f, gradf, Hessf, kmax, tolgrad, c1, rho, btmax, fterms_lin, pcg_maxit);
 
-disp(['Modified Newton converged in ', num2str(k), ' iterations.'])
+disp(['TRUNCATED Newton converged in ', num2str(k), ' iterations.'])
 disp(['The x value at the solution is ', mat2str(xk), '.'])
 disp(['The function value at the solution is ', mat2str(fk), '.'])
