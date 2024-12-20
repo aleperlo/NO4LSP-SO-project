ca0d4589b11021fd0d5bb7f5e9a677e7e80493c9
Alessandro Perlo <aleperlo002@gmail.com>
2024-12-20 15:25:48 +0100

    Fixed cholesky

--- numerical-optimization/scripts/chol_with_addition.m
+++ numerical-optimization/scripts/chol_with_addition.m
@@ -10,14 +10,17 @@ sizes = size(A);
 n = sizes(1);
 
 for i = 1:max_iter
-    try L = chol(A + tau*eye(n));
-        % if i > 1
-        %     disp(['Choosing tau', num2str(tau)])
-        % end
-        return
+    try L = chol(A + tau*speye(n));
+        if i > 1
+            disp(['Choosing tau ', num2str(tau), ' after iterations ', num2str(i), '.'])
+        end
+        break
     catch
         tau = max(coeffient*tau, beta);
     end
+end
+if i == max_iter
+    disp('Tau could not be found!')
 end
 
 end
