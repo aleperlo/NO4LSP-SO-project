4a0134815c0e47b3f82a583559cad4b9e823a16f
Mattia Molinari <matty.molinari@gmail.com>
2025-01-03 16:01:30 +0100

    test function and main script

--- numerical-optimization/scripts/main.m
+++ numerical-optimization/scripts/main.m
@@ -1,72 +1,19 @@
-%% LOADING THE VARIABLES FOR THE TEST
-
-clear
-close all
-clc
+clear all; close all; clc;
 
 load('data/test_functions2.mat');
+load('data/forcing_terms.mat');
 
-beta = 10^-3;
-max_chol_iter = 10^3;
-x0 = [1.2; 1.2];
-
-%% RUN THE STEEPEST DESCENT
-
-disp('**** STEEPEST DESCENT (BACKTRACK + FLAG): START *****')
-
-[xk, fk, gradfk_norm, k, xseq, btseq] = ...
-    modified_newton(x0, f2, gradf2, Hessf2, beta,...
-    kmax, tolgrad, c1, rho, btmax, max_chol_iter);
-disp('**** MODIFIED NEWTON DESCENT: FINISHED *****')
-disp('**** MODIFIED NEWTON: RESULTS *****')
-disp('************************************')
-disp(['xk: ', mat2str(xk), ' (actual minimum: [0; 0]);'])
-disp(['f(xk): ', num2str(fk), ' (actual min. value: 0);'])
-disp('************************************')
+% Seed for reproducibility
+seed = min([331794 337131 338682]);
+rng(seed);
 
 
-%% PLOTS (BACKTRACK)
+disp('**** CHAINED ROSENBROCK *****')
+test(@(x) chained_rosenbrock(x), @(x) chained_rosenbrock_gradf(x),...
+    @(x)chained_rosenbrock_Hessf(x), @(n) chained_rosenbrock_initializer(n),...
+    5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterms_suplin, 100);
 
-f_meshgrid = @(X,Y)reshape(f2([X(:),Y(:)]'),size(X));
-
-% Creation of the meshgrid for the contour-plot
-[X, Y] = meshgrid(linspace(-6, 6, 500), linspace(-6, 6, 500));
-% Computation of the values of f for each point of the mesh
-Z = f_meshgrid(X, Y);
-
-% Plots
-
-% Simple Plot
-fig1 = figure();
-% Contour plot with curve levels for each point in xseq
-[C1, ~] = contour(X, Y, Z);
-hold on
-% plot of the points in xseq
-plot(xseq(1, :), xseq(2, :), '--*')
-hold off
-
-% More interesting Plot
-fig2 = figure();
-% Contour plot with curve levels for each point in xseq
-% ATTENTION: actually, the mesh [X, Y] is too coarse for plotting the last
-% level curves corresponding to the last point in xseq (check it zooming
-% the image).
-[C2, ~] = contour(X, Y, Z, f2(xseq));
-hold on
-% plot of the points in xseq
-plot(xseq(1, :), xseq(2, :), '--*')
-hold off
-
-% Barplot of btseq
-fig3 = figure();
-bar(btseq)
-
-% Much more interesting plot
-fig4 = figure();
-surf(X, Y, Z,'EdgeColor','none')
-hold on
-plot3(xseq(1, :), xseq(2, :), f2(xseq), 'r--*')
-hold off
-
-
-
+% disp('**** DISCRETE BOUNDARY *****')
+% test(@(x) discrete_boundary(x), @(x) discrete_boundary_gradf(x),...
+%     @(x)discrete_boundary_Hessf(x), @(n) chained_rosenbrock_initializer(n),...
+%     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterms_quad, 100);
---
+++ numerical-optimization/scripts/test.m
@@ -0,0 +1,60 @@
+function [] = test(f, gradf, Hessf, initializer,...
+    kmax, tolgrad, c1, rho, btmax, chol_maxit, beta, fterms, pcg_maxit)
+%
+% INPUTS
+% n = dimension of the problem;
+% f = function handle that describes a function R^n->R;
+% gradf = function handle that describes the gradient of f;
+% Hessf = function handle that describes the Hessian of f;
+% initializer = function handle that generate starting point x0;
+% kmax = maximum number of iterations permitted;
+% tolgrad = value used as stopping criterion w.r.t. the norm of the gradient;
+% c1 = the factor of the Armijo condition that must be a scalar in (0,1);
+% rho = fixed factor, lesser than 1, used for reducing alpha0;
+% btmax = maximum number of steps for updating alpha during the backtracking strategy.
+% chol_maxit = maximum number of iterations for the Cholesky factorization
+% beta = fixed factor, greater than 1, used for the Cholesky factorization;
+% fterms = function handle taking as input arguments k and gradfk, and returning the forcing term etak
+% pcg_maxit = maximum number of iterations for the pcg solver
+% h = step size for the finite difference approximation of the gradient and Hessian.
+%
+% OUTPUTS
+%
+for i=3:5
+    % Dimension of the problem
+    n=10^i;
+    % Initialization of the starting point
+    x0 = initializer(n);
+
+    % Run the optimization algorithm for 11 different starting points
+    for j=0:10
+        if j == 0
+            x0_j = x0;
+        else
+            x0_j = 2 * rand(n, 1) + x0 - 1;
+        end
+        disp(['**** STARTING POINT: (', num2str(i), ', ', num2str(j), ') ****'])
+        disp('**** EXACT GRADIENT AND HESSIAN ****')
+        % Exact gradient and Hessian
+        run_optimization(x0_j, f, gradf, Hessf, beta, kmax, tolgrad, c1, rho, btmax, chol_maxit, fterms, pcg_maxit);
+
+        % Finite difference gradient and Hessian for different values of h
+        % for k=2:2:12
+        %     h = 10^(-k);
+
+        %     % Absolute
+        %     disp(['**** ABSOLUTE FINITE DIFFERENCE GRADIENT AND HESSIAN - h=', num2str(h), ' ****'])
+        %     findiff_gradf = @(x) findiff_grad(f, x, h, 'fw', false);
+        %     run_optimization(x0_j, f, findiff_gradf, @(x) findiff_banded(findiff_gradf, x, h, 3, false), beta, kmax, tolgrad, c1, rho, btmax, chol_maxit, fterms, pcg_maxit);
+
+        %     % % Relative
+        %     % disp(['**** RELATIVE FINITE DIFFERENCE GRADIENT AND HESSIAN - h=', num2str(h), ' ****'])
+        %     % run(x0_j, f, @(x) findiff_grad(f, x, h, 'fw', true ), @(x) findiff_banded(f, x, h, 3, true ), beta, kmax, tolgrad, c1, rho, btmax, chol_maxit, fterms, pcg_maxit);
+
+        % end
+    end
+end
+
+
+
+end
