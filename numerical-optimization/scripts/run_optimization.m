e607d249b3eb7474ca4e65fae386d06d5a8caeac
Mattia Molinari <matty.molinari@gmail.com>
2025-02-01 10:39:33 +0100

    better log

--- numerical-optimization/scripts/run_optimization.m
+++ numerical-optimization/scripts/run_optimization.m
@@ -1,22 +1,23 @@
 function [] = run_optimization(x0, f, gradf, Hessf, beta,...
     kmax, tolgrad, c1, rho, btmax, chol_maxit, fterms, pcg_maxit)
 
-disp('  MODIFIED NEWTON *****')
+disp([9, 9, 9, 'MODIFIED NEWTON *****'])
 [~, fk, gradfk_norm, k, ~, ~] = ...
     modified_newton(x0, f, gradf, Hessf, beta,...
     kmax, tolgrad, c1, rho, btmax, chol_maxit, false);
-disp(['  f(xk): ', num2str(fk)])
-disp(['  gradfk_norm: ', num2str(gradfk_norm)])
-disp(['  k: ', num2str(k)])
-disp('  ********************************')
+disp([9, 9, 9, 'f(xk): ', num2str(fk)])
+disp([9, 9, 9, 'gradfk_norm: ', num2str(gradfk_norm)])
+disp([9, 9, 9, 'k: ', num2str(k)])
+disp([9, 9, 9, '********************************'])
 
-disp('  TRUNCATED NEWTON')
+disp([9, 9, 9, 'TRUNCATED NEWTON'])
 [~, fk, gradfk_norm, k, ~, ~] = ...
     truncated_newton(x0, f, gradf, Hessf, ...
     kmax, tolgrad, c1, rho, btmax, fterms, pcg_maxit, false);
-disp(['  f(xk): ', num2str(fk)])
-disp(['  gradfk_norm: ', num2str(gradfk_norm)])
-disp(['  k: ', num2str(k)])
-disp('  ********************************')
+disp([9, 9, 9, 'f(xk): ', num2str(fk)])
+disp([9, 9, 9, 'gradfk_norm: ', num2str(gradfk_norm)])
+disp([9, 9, 9, 'k: ', num2str(k)])
+disp([9, 9, 9, '********************************'])
+
 end
 
