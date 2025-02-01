669c36019b4f94f2a8c4e094e51c306d1fb3744a
Mattia Molinari <matty.molinari@gmail.com>
2025-02-01 10:40:39 +0100

    updated main

--- numerical-optimization/scripts/main.m
+++ numerical-optimization/scripts/main.m
@@ -1,19 +1,39 @@
-clear all; close all; clc;
+clear all; close all; clc; %#ok<CLALL>
 
 load('data/test_functions2.mat');
 load('data/forcing_terms.mat');
 
 % Seed for reproducibility
 seed = min([331794 337131 338682]);
-rng(seed);
+
+% //TODO rosenbrock
+
+fterms = {@(k, gradf) fterms_suplin(k, gradf), @(k, gradf) fterms_quad(k, gradf)};
+
+for i = 1:length(fterms)
+
+    fterm = fterms{i};
+
+    rng(seed);
+    disp('**** CHAINED ROSENBROCK *****')
+    codiags = 1;
+    test(@(x) chained_rosenbrock(x), @(x) chained_rosenbrock_gradf(x),...
+        @(x)chained_rosenbrock_Hessf(x), @(n) chained_rosenbrock_initializer(n), codiags,...
+        5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
+
+    % rng(seed);
+    % disp('**** GENERALIZED BROYDEN *****')
+    % codiags = 2;
+    % test(@(x) generalized_broyden(x), @(x) generalized_broyden_gradf(x),...
+    %     @(x)generalized_broyden_Hessf(x), @(n) generalized_broyden_initializer(n), codiags,...
+    %     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
 
 
-disp('**** CHAINED ROSENBROCK *****')
-test(@(x) chained_rosenbrock(x), @(x) chained_rosenbrock_gradf(x),...
-    @(x)chained_rosenbrock_Hessf(x), @(n) chained_rosenbrock_initializer(n),...
-    5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterms_suplin, 100);
+    % rng(seed);
+    % disp('**** BANDED TRIGONOMETRIC *****')
+    % codiags = 0;
+    % test(@(x) banded_trigonometric(x), @(x) banded_trigonometric_gradf(x),...
+    %     @(x)banded_trigonometric_Hessf(x), @(n) banded_trigonometric_initializer(n), codiags,...
+    %     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
 
-% disp('**** DISCRETE BOUNDARY *****')
-% test(@(x) discrete_boundary(x), @(x) discrete_boundary_gradf(x),...
-%     @(x)discrete_boundary_Hessf(x), @(n) chained_rosenbrock_initializer(n),...
-%     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterms_quad, 100);
+end
