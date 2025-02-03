65d099916506f5de904a272a72aaabb0397fe4c2
Alessandro Perlo <aleperlo002@gmail.com>
2025-02-03 18:30:10 +0100

    Got rid of fterms

--- numerical-optimization/scripts/main.m
+++ numerical-optimization/scripts/main.m
@@ -8,32 +8,26 @@ seed = min([331794 337131 338682]);
 
 % //TODO rosenbrock
 
-fterms = {@(k, gradf) fterms_suplin(k, gradf), @(k, gradf) fterms_quad(k, gradf)};
+fterm = @(k, gradf) fterms_suplin(k, gradf);
 
-for i = 1:length(fterms)
+rng(seed);
+disp('**** CHAINED ROSENBROCK *****')
+codiags = 1;
+test(@(x) chained_rosenbrock(x), @(x) chained_rosenbrock_gradf(x),...
+    @(x)chained_rosenbrock_Hessf(x), @(n) chained_rosenbrock_initializer(n), codiags,...
+    5e4, 1e-6, 1e-4, 0.5, 100, 1000, 1e-3, fterm, 100, '../results/chained_rosenbrock/');
 
-    fterm = fterms{i};
-
-    rng(seed);
-    disp('**** CHAINED ROSENBROCK *****')
-    codiags = 1;
-    test(@(x) chained_rosenbrock(x), @(x) chained_rosenbrock_gradf(x),...
-        @(x)chained_rosenbrock_Hessf(x), @(n) chained_rosenbrock_initializer(n), codiags,...
-        5e4, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100, '../results/chained_rosenbrock/');
-
-    % rng(seed);
-    % disp('**** GENERALIZED BROYDEN *****')
-    % codiags = 2;
-    % test(@(x) generalized_broyden(x), @(x) generalized_broyden_gradf(x),...
-    %     @(x)generalized_broyden_Hessf(x), @(n) generalized_broyden_initializer(n), codiags,...
-    %     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
+% rng(seed);
+% disp('**** GENERALIZED BROYDEN *****')
+% codiags = 2;
+% test(@(x) generalized_broyden(x), @(x) generalized_broyden_gradf(x),...
+%     @(x)generalized_broyden_Hessf(x), @(n) generalized_broyden_initializer(n), codiags,...
+%     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
 
 
-    % rng(seed);
-    % disp('**** BANDED TRIGONOMETRIC *****')
-    % codiags = 0;
-    % test(@(x) banded_trigonometric(x), @(x) banded_trigonometric_gradf(x),...
-    %     @(x)banded_trigonometric_Hessf(x), @(n) banded_trigonometric_initializer(n), codiags,...
-    %     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
-
-end
+% rng(seed);
+% disp('**** BANDED TRIGONOMETRIC *****')
+% codiags = 0;
+% test(@(x) banded_trigonometric(x), @(x) banded_trigonometric_gradf(x),...
+%     @(x)banded_trigonometric_Hessf(x), @(n) banded_trigonometric_initializer(n), codiags,...
+%     5000, 1e-6, 1e-4, 0.8, 100, 1000, 1e-3, fterm, 100);
