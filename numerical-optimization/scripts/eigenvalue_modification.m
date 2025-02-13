7392ea8d06974ddd1d51a28329306c4a084207cf
Alessandro Perlo <aleperlo002@gmail.com>
2025-02-13 10:47:25 +0100

    Removed unused functions

--- numerical-optimization/scripts/eigenvalue_modification.m
+++
@@ -1,6 +0,0 @@
-function [B] = eigenvalue_modification(Hessfk)
-    delta = sqrt(eps);
-    smallest_eigenvalue = eigs(Hessfk, 1, 'smallestreal');
-    tau = max([0, delta - smallest_eigenvalue]);
-    B = speye(size(Hessfk)) * tau;
-end
